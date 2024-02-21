bed3bc6d6c986463266f01e4ae73e8da043884ae
kahwaipd <100838692+kahwaipd@users.noreply.github.com>
2024-02-21 14:00:09 +0800

    Feat/change virtual dao (#5)
    * allow claim all virtual rewards
    
    * added elo rating
    
    * allow change of impl contracts

--- scripts/deployVirtualImplementations.ts
+++ scripts/deployVirtualImplementations.ts
@@ -3,9 +3,11 @@ import { ethers } from "hardhat";
 (async () => {
   try {
     const dao = await ethers.deployContract("PersonaDAO");
+    await dao.waitForDeployment();
     console.log("PersonaDAO deployed to:", dao.target);
 
     const token = await ethers.deployContract("PersonaToken");
+    await token.waitForDeployment();
     console.log("PersonaToken deployed to:", token.target);
   } catch (e) {
     console.log(e);
--- scripts/upgradeFactory.ts
+++ scripts/upgradeFactory.ts
@@ -3,8 +3,15 @@ import { ethers, upgrades } from "hardhat";
 (async () => {
   try {
     const PersonaFactory = await ethers.getContractFactory("PersonaFactory");
-    const factory = await upgrades.upgradeProxy(process.env.VIRTUAL_FACTORY, PersonaFactory);
-    console.log("Upgraded", factory.target)
+    const factory = await upgrades.upgradeProxy(
+      process.env.VIRTUAL_FACTORY,
+      PersonaFactory
+    );
+    await factory.setImplementations(
+      process.env.VIRTUAL_TOKEN_IMPL,
+      process.env.VIRTUAL_DAO_IMPL
+    );
+    console.log("Upgraded", factory.target);
   } catch (e) {
     console.log(e);
   }
