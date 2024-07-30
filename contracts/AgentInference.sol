6d8ad486fccd2f6008ebc4ba08da66200e169f7c
kw <kahwai@pathdao.io>
2024-07-30 14:47:15 +0800

    updated inference method

--- contracts/AgentInference.sol
+++ contracts/AgentInference.sol
@@ -18,27 +18,23 @@ contract AgentInference is
     using SafeERC20 for IERC20;
 
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
-    uint256 public minFees;
-    mapping(uint256 => uint256) public fees; // Agent custom fees
-    mapping(uint256 => uint256) public inferenceCount; // Inference count per agent
+    mapping(uint256 agentId => uint256) public inferenceCount; // Inference count per agent
 
     IERC20 public token;
     IAgentNft public agentNft;
 
-    event MinFeesUpdated(uint256 minFees);
-    event FeesUpdated(uint256 indexed agentId, uint256 fees);
     event Prompt(
         address indexed sender,
         bytes32 promptHash,
         uint256 agentId,
-        uint256 cost
+        uint256 cost,
+        uint8[] coreIds
     );
 
     function initialize(
         address defaultAdmin_,
         address token_,
-        address agentNft_,
-        uint256 minFees_
+        address agentNft_
     ) external initializer {
         __AccessControl_init();
         __ReentrancyGuard_init();
@@ -47,45 +43,37 @@ contract AgentInference is
         _grantRole(DEFAULT_ADMIN_ROLE, defaultAdmin_);
 
         token = IERC20(token_);
-        minFees = minFees_;
         agentNft = IAgentNft(agentNft_);
-    }
-
-    function setFees(
-        uint256 agentId,
-        uint256 amount
-    ) public onlyRole(ADMIN_ROLE) {
-        fees[agentId] = amount;
-        emit FeesUpdated(agentId, amount);
-    }
-
-    function setMinFees(uint256 amount) public onlyRole(ADMIN_ROLE) {
-        minFees = amount;
-        emit MinFeesUpdated(amount);
     }
 
     function prompt(
         bytes32 promptHash,
-        uint256[] memory agentIds
+        uint256[] memory agentIds,
+        uint256[] memory amounts,
+        uint8[][] memory coreIds
     ) public nonReentrant {
         address sender = _msgSender();
         uint256 total = 0;
 
-        for (uint256 i = 0; i < agentIds.length; i++) {
-            uint256 agentId = agentIds[i];
-            uint256 agentFees = fees[agentId].max(minFees);
-            total += agentFees;
+        require(
+            agentIds.length == amounts.length &&
+                agentIds.length == coreIds.length,
+            "Invalid input"
+        );
+
+        for (uint256 i = 0; i < amounts.length; i++) {
+            total += amounts[i];
         }
 
         require(token.balanceOf(sender) >= total, "Insufficient balance");
 
         for (uint256 i = 0; i < agentIds.length; i++) {
             uint256 agentId = agentIds[i];
-            uint256 agentFees = fees[agentId].max(minFees);
             address agentTba = agentNft.virtualInfo(agentId).tba;
-            token.safeTransferFrom(sender, agentTba, agentFees);
+            token.safeTransferFrom(sender, agentTba, amounts[i]);
+
             inferenceCount[agentId]++;
-            emit Prompt(sender, promptHash, agentId, agentFees);
+            emit Prompt(sender, promptHash, agentId, amounts[i], coreIds[i]);
         }
     }
 }
